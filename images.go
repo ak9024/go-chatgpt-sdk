bdf4ddde88bb5f95e2ab27a4ff261c75e4c1b6c6
Adiatma Kamarudin <adiatma.mail@gmail.com>
2023-07-24 07:49:43 +0800

    feat(images): add error handling for HTTP status code
    Add error handling for HTTP status code in the ImagesGenerations, ImagesGenerationsB64JSON, ImagesVariations, and ImagesVariationsB64JSON functions. If the HTTP status code is not 200 OK, the functions will return an ErrorResponse object with the error message.
    
    In the ImagesGenerations function, if the request fails, the function will return the ErrorResponse object instead of just nil. Similarly, in the ImagesGenerationsB64JSON, ImagesVariations, and ImagesVariationsB64JSON functions, the ErrorResponse object is returned in case of failure.
    
    Additionally, when making the HTTP request, the functions now set the Error field in the ErrorResponse object using the SetError method.
    
    Lastly, after making the HTTP request, if the status code is not 200 OK, the functions return the ErrorResponse object instead of nil.
    
    This change improves the error handling in the images package and provides more informative error messages when encountering non-200 HTTP

--- images.go
+++ images.go
@@ -1,51 +1,69 @@
 package gochatgptsdk
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-func (c *chatgpt) ImagesGenerations(b ModelImages) (*ModelImagesResponse[DataURL], error) {
+func (c *chatgpt) ImagesGenerations(b ModelImages) (*ModelImagesResponse[DataURL], *ErrorResponse) {
 	// POST https://api.openai.com/v1/images/generations
 	endpointImagesGeneratios := fmt.Sprintf("%s/images/generations", ChatGPTAPIV1)
 
 	result := ModelImagesResponse[DataURL]{}
+	errorResponse := ErrorResponse{}
 
-	_, err := DoRequest(c.OpenAIKey).
+	resp, err := DoRequest(c.OpenAIKey).
 		SetBody(b).
 		SetResult(&result).
+		SetError(&errorResponse).
 		Post(endpointImagesGeneratios)
 	if err != nil {
-		return nil, err
+		return nil, &errorResponse
+	}
+
+	// if status code not 200 ok, please send error message
+	if resp.StatusCode() != http.StatusOK {
+		return nil, &errorResponse
 	}
 
 	return &result, nil
 }
 
-func (c *chatgpt) ImagesGenerationsB64JSON(b ModelImages) (*ModelImagesResponse[DataB64JSON], error) {
+func (c *chatgpt) ImagesGenerationsB64JSON(b ModelImages) (*ModelImagesResponse[DataB64JSON], *ErrorResponse) {
 	// POST https/api.openai.com/v1/images/generations
 	endpointImagesGeneratios := fmt.Sprintf("%s/images/generations", ChatGPTAPIV1)
 
 	result := ModelImagesResponse[DataB64JSON]{}
+	errorResponse := ErrorResponse{}
 
-	_, err := DoRequest(c.OpenAIKey).
+	resp, err := DoRequest(c.OpenAIKey).
 		SetBody(b).
 		SetResult(&result).
+		SetError(&errorResponse).
 		Post(endpointImagesGeneratios)
 	if err != nil {
-		return nil, err
+		return nil, &errorResponse
+	}
+
+	// if status code not 200 ok, please send error message
+	if resp.StatusCode() != http.StatusOK {
+		return nil, &errorResponse
 	}
 
 	return &result, nil
 }
 
-func (c *chatgpt) ImagesVariations(b ModelImagesVariations) (*ModelImagesResponse[DataURL], error) {
+func (c *chatgpt) ImagesVariations(b ModelImagesVariations) (*ModelImagesResponse[DataURL], *ErrorResponse) {
 	// POST https/api.openai.com/v1/images/variations
 	endpointImagesVariations := fmt.Sprintf("%s/images/variations", ChatGPTAPIV1)
 
 	result := ModelImagesResponse[DataURL]{}
+	errResponse := ErrorResponse{}
 
 	// set default images variations
 	d := defaultImagesVariations(b, ResponseFormatURL)
 
-	_, err := DoRequest(c.OpenAIKey).
+	resp, err := DoRequest(c.OpenAIKey).
 		SetFile("image", *&d.Image).
 		SetFormData(map[string]string{
 			"n":               d.N,
@@ -54,25 +72,32 @@ func (c *chatgpt) ImagesVariations(b ModelImagesVariations) (*ModelImagesRespons
 			"user":            d.User,
 		}).
 		SetResult(&result).
+		SetError(&errResponse).
 		Post(endpointImagesVariations)
 	if err != nil {
-		return nil, err
+		return nil, &errResponse
+	}
+
+	// if status code not 200 ok, please send error message
+	if resp.StatusCode() != http.StatusOK {
+		return nil, &errResponse
 	}
 
 	return &result, nil
 }
 
-func (c *chatgpt) ImagesVariationsB64JSON(b ModelImagesVariations) (*ModelImagesResponse[DataB64JSON], error) {
+func (c *chatgpt) ImagesVariationsB64JSON(b ModelImagesVariations) (*ModelImagesResponse[DataB64JSON], *ErrorResponse) {
 	// POST https/api.openai.com/v1/images/variations
 	endpointImagesVariations := fmt.Sprintf("%s/images/variations", ChatGPTAPIV1)
 
 	result := ModelImagesResponse[DataB64JSON]{}
+	errResponse := ErrorResponse{}
 
 	// set default images variations
 	d := defaultImagesVariations(b, ResponseFormatB64JSON)
 
-	_, err := DoRequest(c.OpenAIKey).
-		SetFile("image", *&b.Image).
+	resp, err := DoRequest(c.OpenAIKey).
+		SetFile("image", *&d.Image).
 		SetFormData(map[string]string{
 			"n":               d.N,
 			"size":            d.Size,
@@ -80,9 +105,15 @@ func (c *chatgpt) ImagesVariationsB64JSON(b ModelImagesVariations) (*ModelImages
 			"user":            d.User,
 		}).
 		SetResult(&result).
+		SetError(&errResponse).
 		Post(endpointImagesVariations)
 	if err != nil {
-		return nil, err
+		return nil, &errResponse
+	}
+
+	// if status code not 200 ok, please send error message
+	if resp.StatusCode() != http.StatusOK {
+		return nil, &errResponse
 	}
 
 	return &result, nil
