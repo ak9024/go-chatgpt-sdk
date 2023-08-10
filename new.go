350a05bcad799f511ee91b29097fe33f2542cb7f
Adiatma Kamarudin <adiatma.mail@gmail.com>
2023-08-10 18:24:39 +0800

    chore(new): refactor Chatgpt interface and NewConfig function
    Refactor the Chatgpt interface to include additional methods for generating images and variations of images. This allows for more capabilities in the Chatgpt package. The NewConfig function is also updated to return an instance of the Chatgpt interface instead of the specific struct type. The OpenAIKey field remains unchanged.

--- new.go
+++ new.go
@@ -8,7 +8,16 @@ type Config struct {
 	OpenAIKey string
 }
 
-func NewConfig(c Config) *chatgpt {
+type Chatgpt interface {
+	ChatCompletions(b ModelChat) (*ModelChatResponse, *ErrorResponse)
+	Completions(b ModelText) (*ModelTextResponse, *ErrorResponse)
+	ImagesGenerations(b ModelImages) (*ModelImagesResponse[DataURL], *ErrorResponse)
+	ImagesGenerationsB64JSON(b ModelImages) (*ModelImagesResponse[DataB64JSON], *ErrorResponse)
+	ImagesVariations(b ModelImagesVariations) (*ModelImagesResponse[DataURL], *ErrorResponse)
+	ImagesVariationsB64JSON(b ModelImagesVariations) (*ModelImagesResponse[DataB64JSON], *ErrorResponse)
+}
+
+func NewConfig(c Config) Chatgpt {
 	return &chatgpt{
 		OpenAIKey: c.OpenAIKey,
 	}
